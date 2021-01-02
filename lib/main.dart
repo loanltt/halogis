dace65a406496891cb863e440767cc40a8556ab3
loanltt <loanlethi2309@gmail.com>
2021-01-02 19:06:52 +0700

    Added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,400 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  //runApp(DangNhap());
+  runApp(MaterialApp(
+    title: 'Named Routes Demo',
+    // Start the app with the "/" named route. In this case, the app starts
+    // on the FirstScreen widget.
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => DangNhap(),
+      '/dangky': (context) => DangKy(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/thongtinnguoidung': (context) => Thongtinnguoidung(),
+      // /name: (context) => NameScreen(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class DangNhap extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      //title: 'OTO VIET',
+      home: Scaffold(
+        /*appBar: AppBar(
+          title: Text('OTO VIET'),
+        ),*/
+        body: ListView(
+          children: [
+            textLogin,
+            tfLogin,
+            textPassword,
+            tfPassword,
+            buttonLogin,
+            forgetpassword,
+            //signIn
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(16),
+              child:GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, "/dangky");
+                },
+                child: Text(
+                  'Chưa có tài khoản? Đăng ký!',
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            )
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget textLogin = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Tên đăng nhập',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget forgetpassword = Container(
+    alignment: Alignment.center,
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Quên mật khẩu',
+      softWrap: true,
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 17,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+  );
+
+  Widget signIn = Container(
+    alignment: Alignment.center,
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Chưa có tài khoản? Đăng ký!',
+      softWrap: true,
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 17,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+  );
+
+
+  Widget tfLogin = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Số điện thoại hoặc email',
+      ),
+    ),
+  );
+
+  Widget textPassword = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Mật Khẩu',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfPassword = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Mật Khẩu',
+      ),
+    ),
+  );
+
+  Widget buttonLogin = Container(
+    padding: const EdgeInsets.all(32),
+    child: RaisedButton(
+      //     disabledColor: Colors.red,
+      // disabledTextColor: Colors.black,
+      padding: const EdgeInsets.all(20),
+      textColor: Colors.white,
+      color: Colors.blue,
+      onPressed: () {
+
+      },
+      child: Text('Đăng nhập'),
+    ),
+  );
+
+}
+
+class DangKy extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OTO VIET',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('OTO VIET'),
+        ),
+        body: ListView(
+          children: [
+            //textLogin
+            textSignIn,
+            tfSignIn,
+            textPassword,
+            tfPassword,
+            checkedpassword,
+            tfcheckedpassword,
+            RaisedButton(
+              //     disabledColor: Colors.red,
+              // disabledTextColor: Colors.black,
+              padding: const EdgeInsets.all(20),
+              textColor: Colors.white,
+              color: Colors.blue,
+              onPressed: () {
+                Navigator.pushNamed(context, '/thongtinnguoidung');
+              },
+              child: Text('Tiếp tục'),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textSignIn = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Tài khoản',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfSignIn = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Số điện thoại hoặc email',
+      ),
+    ),
+  );
+
+  Widget textPassword = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Mật Khẩu',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfPassword = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Mật Khẩu',
+      ),
+    ),
+  );
+
+  Widget checkedpassword = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Nhập lại mật khẩu',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfcheckedpassword = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Mật Khẩu',
+      ),
+    ),
+  );
+
+  Widget buttonNext = Container(
+    padding: const EdgeInsets.all(32),
+    child: RaisedButton(
+      //     disabledColor: Colors.red,
+      // disabledTextColor: Colors.black,
+      padding: const EdgeInsets.all(20),
+      textColor: Colors.white,
+      color: Colors.blue,
+      onPressed: () {
+        //Navigator.pushNamed(context, '/second');
+      },
+      child: Text('Tiếp tục'),
+    ),
+  );
+}
+
+class Thongtinnguoidung extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OTO VIET',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('OTO VIET'),
+        ),
+        body: ListView(
+          children: [
+            //textLogin
+            textYourName,
+            tfYourName,
+            textCmnd,
+            tfCmnd,
+            textDate,
+            tfDate,
+            RaisedButton(
+              //     disabledColor: Colors.red,
+              // disabledTextColor: Colors.black,
+              padding: const EdgeInsets.all(20),
+              textColor: Colors.white,
+              color: Colors.blue,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Xong'),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textYourName = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Họ và Tên',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfYourName = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Họ và Tên',
+      ),
+    ),
+  );
+
+  Widget textCmnd = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Số chứng minh nhân dân',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfCmnd = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Nhập số chứng minh nhân dân',
+      ),
+    ),
+  );
+
+  Widget textDate = Container(
+    padding: const EdgeInsets.all(16),
+    child: Text(
+      'Ngày tháng năm sinh',
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17,
+          fontWeight: FontWeight.w500
+      ),
+    ),
+  );
+
+  Widget tfDate = Container(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Nhập ngày tháng năm sinh',
+      ),
+    ),
+  );
+
+  Widget buttonEnter = Container(
+    padding: const EdgeInsets.all(32),
+    child: RaisedButton(
+      //     disabledColor: Colors.red,
+      // disabledTextColor: Colors.black,
+      padding: const EdgeInsets.all(20),
+      textColor: Colors.white,
+      color: Colors.blue,
+      onPressed: () {
+
+      },
+      child: Text('Xong'),
+    ),
+  );
 }
